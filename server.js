49b02a0c3327fc1a2d5a150cb306105ca792456a
charu9843 <114791511+charu9843@users.noreply.github.com>
2025-08-12 20:20:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const openai = new OpenAI({
 
 // Middleware
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, 'index.html')));
 
 
 
@@ -172,7 +172,7 @@ app.get('/download', async (req, res) => {
   await archive.finalize();
 });
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+  res.sendFile(path.join(__dirname,  'index.html'));
 });
 app.get('/test', (req, res) => {
   res.send('Server is running!');
